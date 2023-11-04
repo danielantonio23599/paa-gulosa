4070e3603ca170e164d7e9d845cb7bd080d14737
Fabrica CW - Daniel <fabrica.formiga@basis.com.br>
2023-11-04 13:54:15 -0300

    versão 1.0

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from solver import Solver
-
-
 print(' Problema BeeCrowd Programação Gulosa ver 1.0 - IFMG 2023')
 print('Desenvolvido como trabalho prático para a disciplina de PAA')
 print('Autores: Daniel Antônio de Sá')
-# Entrada
-linha2 = input("Digite a quantidade de digitos do (numero) seguido pela quantidadee que serão removidos").strip()
-linha = input("Digite o numero").strip()
-solver = Solver()
-# Saída
-print(solver.maior_numero_possivel(linha, linha2[0]))
+while True:
+    N, D = map(int, input().split()) # entrada dos numeros e digitos
+    if N == 0 and D == 0:
+        break
+    num = input().strip()
+    final = [] # array de numeros final
+    removido = 0
+    for digito in num: # percorre o vetor
+        # se a quantidade de removidos tiver chagado até a D = a necessaria e
+        # final for diferente de vazio e a ultima posição do final form menor digito
+        while removido < D and final and final[-1] < digito:
+            final.pop()
+            removido += 1
+        final.append(digito)
+
+    # Se ainda for necessario remover mais dígitos, remova a partir do final do vetor
+    while removido < D:
+        final.pop()
+        removido += 1
+    print("".join(final))
--- solver.py
+++
@@ -1,5 +0,0 @@
-class Solver:
-
-    def maior_numero_possivel(self, numero, digitos):
-
-        return numero
